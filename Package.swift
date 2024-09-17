29442654211b667ab18a6e8ac7117195a025d5df
Andreas Bauer <andreas.bauer@stanford.edu>
2024-09-17 11:44:05 +0200

    Enable Swift 6 language mode

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 
 //
 // This source file is part of the Stanford SpeziDevices open source project
-// 
+//
 // SPDX-FileCopyrightText: 2022 Stanford University and the project authors (see CONTRIBUTORS.md)
-// 
+//
 // SPDX-License-Identifier: MIT
 //
 
